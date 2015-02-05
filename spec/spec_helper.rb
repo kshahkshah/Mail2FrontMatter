a0db74301370c8094628ee79038fa4f227b0200d
Kunal Shah <me@kunalashah.com>
2015-02-05 11:44:35 -0500

    better setup and cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,40 @@ require File.join(M2FM_GEM_PATH, 'lib', 'mail2frontmatter')
 
 RSpec.configure do |config|
   config.before(:suite) do
+    repo = Rugged::Repository.init_at(File.join(M2FM_GEM_PATH, 'spec', 'installation', '.git'))
+    index = repo.index
+
+    index.add({
+      path: 'data/.gitkeep', 
+      oid: (Rugged::Blob.from_workdir repo, 'data/.gitkeep'),
+      mode: 0100644
+    })
+
+    index.add({
+      path: 'media/.gitkeep', 
+      oid: (Rugged::Blob.from_workdir repo, 'media/.gitkeep'),
+      mode: 0100644
+    })
+
+    # commit
+    tree = index.write_tree(repo)
+    index.write
+
+    author = { 
+      email: 'sender@example.com',
+      name:  'The Sender',
+      time:  Time.now 
+    }
+
+    commit = Rugged::Commit.create(repo, {
+      author:     author,
+      committer:  author,
+      message:    "checking in data/media empties",
+      parents:    [],
+      tree:       tree,
+      update_ref: 'HEAD'
+    })
+
     FileUtils.rm_rf(Dir[File.join(M2FM_GEM_PATH, 'spec', 'installation', 'data', '*')])
     FileUtils.rm_rf(Dir[File.join(M2FM_GEM_PATH, 'spec', 'installation', 'media', '*')])
   end
@@ -15,5 +49,6 @@ RSpec.configure do |config|
   config.after(:suite) do
     FileUtils.rm_rf(Dir[File.join(M2FM_GEM_PATH, 'spec', 'installation', 'data', '*')])
     FileUtils.rm_rf(Dir[File.join(M2FM_GEM_PATH, 'spec', 'installation', 'media', '*')])
+    FileUtils.rm_rf(Dir[File.join(M2FM_GEM_PATH, 'spec', 'installation', '.git')])
   end
 end
